452ee9f39f794956c5e757f4f23452eb18f6375a
Fernando Martinez <fernando817mm@gmail.com>
2021-09-01 15:52:11 -0500

    mvp done

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,66 @@
+const { getById } = require("./../users/users-model");
+const yup = require("yup");
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(`[${req.method}]: [${req.url}]`);
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  const { id } = req.params;
+  getById(id)
+    .then((user) => {
+      user
+        ? ((req.user = user), next())
+        : next({
+            status: 404,
+            message: `user not found`,
+          });
+    })
+    .catch(next);
 }
+
+const userSchema = yup.object({
+  name: yup.string().trim().required(),
+});
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  userSchema
+    .validate(req.body)
+    .then((user) => {
+      req.body = user;
+      next();
+    })
+    .catch(() => {
+      next({
+        status: 400,
+        message: "missing required name field",
+      });
+    });
 }
+
+const postSchema = yup.object({
+  text: yup.string().trim().required(),
+});
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  postSchema
+    .validate(req.body)
+    .then((post) => {
+      req.body = post;
+      next();
+    })
+    .catch(() => {
+      next({
+        status: 400,
+        message: "missing required text field",
+      });
+    });
 }
 
-// do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+};
